5b6d840ec4eb8ef95a2996cf90b5485f2329d690
Kumpob <90686608+Kumpob@users.noreply.github.com>
2026-05-05 01:22:16 +0100

    Update convert_to_png.py

--- convert_to_png.py
+++ convert_to_png.py
@@ -1,27 +1,26 @@
 import os
+import argparse
 from PIL import Image
 
-# Folder containing images
-input_folder = "test"
-# Folder to save converted png images
-output_folder = "test/png"
+parser = argparse.ArgumentParser(description="Convert images to PNG format.")
+parser.add_argument("input_folder", help="Folder containing input images")
+parser.add_argument("output_folder", help="Folder to save converted PNG images")
+args = parser.parse_args()
 
-# Supported input formats
+input_folder = args.input_folder
+output_folder = args.output_folder
+
 supported_formats = (".jpg", ".jpeg", ".bmp", ".tiff", ".webp", ".gif")
 
-# Create output folder if it doesn't exist
 os.makedirs(output_folder, exist_ok=True)
 
-# Loop through all files in the input folder
 for filename in os.listdir(input_folder):
     if filename.lower().endswith(supported_formats):
         input_path = os.path.join(input_folder, filename)
         png_filename = os.path.splitext(filename)[0] + ".png"
         output_path = os.path.join(output_folder, png_filename)
 
-        # Open image and convert to PNG
         with Image.open(input_path) as img:
-            # Convert mode if needed (some images like GIFs may need conversion)
             if img.mode in ("P", "RGBA", "LA"):
                 img = img.convert("RGBA")
             else:
